f4486c0f44d46f67985721ba1584deb74481bf48
John Mario <johnmgc.dev@gmail.com>
2022-07-28 12:34:59 -0500

    prueba

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   # devise_for :users, :controllers => { registrations: 'registrations' }
   
 
-  root  "publishes#index"
+  root "publishes#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
